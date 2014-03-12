5533b70d21099b407c823079d2f7846877588e69
eleanor deal <eleanor.deal@fdmgroup.com>
2014-03-12 18:14:42 +0000

    Database functions moved to summary page, session variables updates

--- lib/CHProject/Controller/ChangeName.pm
+++ lib/CHProject/Controller/ChangeName.pm
@@ -6,52 +6,27 @@ use Mango::BSON qw/ bson_ts /;
 sub changeName{
 	my ($self) = @_;
 
-	print "changename";
-
 	#If user 'Submits' the form
 	if ($self->req->method eq 'POST'){
 		$self->render_later;
 
-		print "check a";
-
 		#Store user inputs to local variables
 		$self->session(
-			oldName=>$self->param('company_name'),
+			oldname=>$self->param('company_name'),
 			id => $self->param('company_id'),
-			newName => $self->param('new_name')
+			newname => $self->param('new_name')
 		);
 
 		my $oldName = $self->param('company_name');
 		my $id = $self->param('company_id');
-		my $newName = $self->param('new_name');
-
+		my $newname = $self->param('new_name');
+		
 		#Check the database for company existance
 		my $doc = $self->db->collection('Companies')->find_one({ _id => $id });
 		my $valid = $doc->{'company name'};
 
-		print $valid;
-		print $oldName;
-
-		#if the company info entered is valid, update the document and redirect
+		#if the company info entered is valid, redirect
 		if($valid eq $oldName){
-			print "check";
-			$self->db->collection('Companies')->update(
-				{ _id => $id },
-				{ '$set' => { "company name" => $newName} },
-			);
-			print "check 2";
-
-			#Add a record of changes made to a separate collection
-			#Will contain standard object IDs for the changes as well as 
-			#a reference to the company ID for continuity across changes
-			$self->db->collection('Changes')->insert(
-				{ 'company id' => $id,
-				  'Old Name' => $oldName,
-				  'Updated Name' => $newName,
-				  'Time' => bson_ts(time) }
-			);
-			print "check 3";
-
 			$self->redirect_to("summary");
 		}
 		#If invalid, try again
--- lib/CHProject/Controller/Summary.pm
+++ lib/CHProject/Controller/Summary.pm
@@ -1,13 +1,50 @@
 package CHProject::Controller::Summary;
 use Mojo::Base 'Mojolicious::Controller';
+use Mango;
+use Mango::BSON qw\ bson_ts \;
 
 sub summary{
 	my $self = shift;
-	my $id = $self->session("id");
-	my $oldname = $self->session("oldname");
-	my $newname = $self->session("newname");
-	
+	my $id = $self->session->{id};
+	my $oldname = $self->session->{oldname};
+	my $newname = $self->session->{newname};
+
+	print $id;
+	print "-----";
+	print $oldname;
+	print "-----";
+	print $newname;
+	print "-----";
+
+
+	#Update the Company Document
+	$self->db->collection('Companies')->update(
+		{ _id => $id },
+		{ '$set' => { "company name" => $newname } },
+	);
+
+	#Add Change Record
+	$self->db->collection('Changes')->insert(
+		{ 'company id' => $id,
+		  'old name' => $oldname,
+		  'updated name' => $newname,
+		  'time' => bson_ts(time) }
+	);
+
+	my $doc = $self->db->collection('Changes')->find_one(
+		{ 'company id' => $id,
+		  'updated name' => $newname }
+	);
+
+	$self->session(
+		changeid => $doc->{'_id'},
+		oldname => $doc->{'old name'},
+		newname => $doc->{'updated name'},
+		id => $id
+	);
+
 	$self->render("summary/summary");
+
 }
 
 1;
