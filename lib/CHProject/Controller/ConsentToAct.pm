dbba4433fc6a3de2627f2bbe2a9c400e878e1439
nathanshumoogum <nathan.shumoogum@FDMGroup.com>
2014-03-18 16:39:38 +0000

    Refactoring of ConsentToAct.pm - reduction in code by using hash for fields and equivalent variables.

--- lib/CHProject/Controller/ConsentToAct.pm
+++ lib/CHProject/Controller/ConsentToAct.pm
@@ -11,61 +11,30 @@ sub consentToAct{
 		$self->render_later;
 	
 		#Setting values from user input on webpage
+		my %fields = (
+			'birth', $self->param('birth'),
+			'phone', $self->param('phone'),
+			'passport', $self->param('passport'),
+			'nin', $self->param('nin'),
+			'maiden', $self->param('maiden'),
+			'eye', $self->param('eye'),
+			'father', $self->param('father')
+		);
 
-		my $birth = $self->param('birth');
-		my $phone = $self->param('phone');
-		my $passport = $self->param('passport');
-		my $nin = $self->param('nin');
-		my $maiden = $self->param('maiden');
-		my $eye = $self->param('eye');
-		my $father = $self->param('father');
+		my @keys = keys %fields;
 		my $id = $self->session->{id};
 		my $count = 0;
 
 		#Validating user input details
 
-		if (defined($birth) && length($birth) == 3){
-			my $result = $self->db->collection('Companies')->find_one({_id => $id}, {cta => 'true'})->{'cta'}->{'birth'};
+		for (my $i = 0; $i < 7; $i++){
+			if (defined($fields{$keys[$i]}) && length($fields{$keys[$i]}) == 3){
+				my $result = $self->db->collection('Companies')->find_one({_id => $id}, {cta => 'true'})->{'cta'}->{$keys[$i]};
 							
-			$count = &check($result, $birth, $count);
+				$count = &check($result, $fields{$keys[$i]}, $count);
+			}
 		}
-
-		if (defined($phone) && length($phone) == 3){
-			my $result = $self->db->collection('Companies')->find_one({_id => $id}, {cta => 'true'})->{'cta'}->{'phone'};
-			
-			$count = &check($result, $phone, $count);
-		}
-
-		if (defined($passport) && length($passport) == 3){
-			my $result = $self->db->collection('Companies')->find_one({_id => $id}, {cta => 'true'})->{'cta'}->{'passport'};
-			
-			$count = &check($result, $passport, $count);
-		}
-
-		if (defined($nin) && length($nin) == 3){
-			my $result = $self->db->collection('Companies')->find_one({_id => $id}, {cta => 'true'})->{'cta'}->{'nin'};
-			
-			$count = &check($result, $nin, $count);
-		}
-
-		if (defined($maiden) && length($maiden) == 3 ){
-			my $result = $self->db->collection('Companies')->find_one({_id => $id}, {cta => 'true'})->{'cta'}->{'maiden'};
-			
-			$count = &check($result, $maiden, $count);
-		}
-
-		if (defined($eye) && length($eye) == 3 ){
-			my $result = $self->db->collection('Companies')->find_one({_id => $id}, {cta => 'true'})->{'cta'}->{'eye'};
-			
-			$count = &check($result, $eye, $count);
-		}
-
-		if (defined($father) && length($father) == 3 ){
-			my $result = $self->db->collection('Companies')->find_one({_id => $id}, {cta => 'true'})->{'cta'}->{'father'};
-			
-			$count = &check($result, $father, $count);
-		}
-
+		
 		#Verifying the user has correctly inputted 3 security questions - if
 		#successful user will be redirected to summary page		
 		
