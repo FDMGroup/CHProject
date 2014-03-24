32b625ea9b476e578e06ff777882e5660b6af496
nathanshumoogum <nathan.shumoogum@FDMGroup.com>
2014-03-24 10:35:58 +0000

    Given names to variables in subroutine check within ConsentToAct.pm

--- lib/CHProject/Controller/ConsentToAct.pm
+++ lib/CHProject/Controller/ConsentToAct.pm
@@ -30,9 +30,10 @@ sub consentToAct{
 			if (defined($fields{$keys[$i]}) && length($fields{$keys[$i]}) == 3){
 				my $result = $self->db->collection('Companies')->find_one(
 					{ _id => $id}, {cta => 'true'})->{'cta'}->{$keys[$i]};
+				my $field = $fields{$keys[$i]};
 				
 				#Calling subroutine &check to validate user input to value stored in database '$result'							
-				$count = &check($result, $fields{$keys[$i]}, $count);
+				$count = &check($result, $field}, $count);
 			}
 		}
 		
@@ -48,9 +49,10 @@ sub consentToAct{
 }
 
 sub check{
-	if($_[0] eq $_[1]){
-		$_[2] ++;
+	my ($result, $field, $count) = @_;
+	if($result eq $field){
+		$count ++;
 	}
-	return $_[2];
+	return $count;
 }
 1;
